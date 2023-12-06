b5ca4d2ba211bad9edf6bc2fff8d7a166b4290a7
FaniryKeziah <hei.keziah.2@gmail.com>
2023-12-06 13:47:36 +0300

    fix: fix bug

--- src/main/java/com/poja/prime/service/event/EvenService.java
+++ src/main/java/com/poja/prime/service/event/EvenService.java
@@ -3,20 +3,12 @@ package com.poja.prime.service.event;
 import org.springframework.stereotype.Service;
 
 import java.math.BigInteger;
-import java.security.SecureRandom;
+import java.util.Random;
 
 @Service
 public class EvenService {
     public BigInteger generatePrime() {
-        SecureRandom random = new SecureRandom();
-
-        BigInteger prime;
-        int certainty = 90;
-
-        do {
-            prime = new BigInteger(10000, random);
-        } while (!prime.isProbablePrime(certainty));
-
-        return prime;
+        Random random = new Random();
+        return BigInteger.probablePrime(10000, random);
     }
 }
